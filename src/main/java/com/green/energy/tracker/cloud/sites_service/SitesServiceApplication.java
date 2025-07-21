c1bc360576f02ca45de5377a21282092d389336b
Andrea Zemignani <andrea.zemignani@acsoftware.it>
2025-07-21 22:05:43 +0200

    initial commit

---
+++ src/main/java/com/green/energy/tracker/cloud/sites_service/SitesServiceApplication.java
@@ -0,0 +1,13 @@
+package com.green.energy.tracker.cloud.sites_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SitesServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SitesServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/green/energy/tracker/cloud/sites_service/SitesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.green.energy.tracker.cloud.sites_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SitesServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
