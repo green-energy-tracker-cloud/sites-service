d87cf8fb36c056245fd8fa970e7f0d4d6ad5129b
Andrea Zemignani <andrea.zemignani@acsoftware.it>
2025-10-09 11:06:25 +0200

    add controller advice and circuit breaker

--- src/main/java/com/green/energy/tracker/cloud/sites_service/repository/SiteRepositoryFirebaseImpl.java
+++ src/main/java/com/green/energy/tracker/cloud/sites_service/repository/SiteRepositoryFirebaseImpl.java
@@ -1,9 +1,7 @@
 package com.green.energy.tracker.cloud.sites_service.repository;
 
 import com.google.cloud.firestore.Firestore;
-import com.green.energy.tracker.cloud.sites_service.exception.FirestoreUnavailableException;
 import com.green.energy.tracker.cloud.sites_service.model.Site;
-import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
@@ -18,19 +16,16 @@ public class SiteRepositoryFirebaseImpl implements SiteRepository{
     private final static String SITES_COLLECTION = "sites";
     private final Firestore firestoreClient;
 
-    @CircuitBreaker(name = "firestoreCb", fallbackMethod = "saveFallback")
     @Override
     public Site save(Site site) throws ExecutionException, InterruptedException {
-        throw new RuntimeException("Simulated Firestore failure");
-        //firestoreClient.collection(SITES_COLLECTION)
-        //        .document(site.getSiteId())
-        //        .set(site)
-        //        .get();
-        //log.info("Site saved with ID: {}", site.getSiteId());
-        //return site;
+        firestoreClient.collection(SITES_COLLECTION)
+                .document(site.getSiteId())
+                .set(site)
+                .get();
+        log.info("Site saved with ID: {}", site.getSiteId());
+        return site;
     }
 
-    @CircuitBreaker(name = "firestoreCb", fallbackMethod = "getByNameFallback")
     @Override
     public Optional<Site> getByName(String name) throws ExecutionException, InterruptedException {
         return firestoreClient.collection(SITES_COLLECTION)
@@ -43,7 +38,6 @@ public class SiteRepositoryFirebaseImpl implements SiteRepository{
                 .findFirst();
     }
 
-    @CircuitBreaker(name = "firestoreCb", fallbackMethod = "updateFallback")
     @Override
     public Optional<Site> update(String name, Site updatedSite) throws ExecutionException, InterruptedException {
         var optSite = getByName(name);
@@ -54,7 +48,6 @@ public class SiteRepositoryFirebaseImpl implements SiteRepository{
         return Optional.of(save(updatedSite));
     }
 
-    @CircuitBreaker(name = "firestoreCb", fallbackMethod = "deleteFallback")
     @Override
     public Boolean delete(String name) throws ExecutionException, InterruptedException {
         var optSite = getByName(name);
@@ -70,7 +63,6 @@ public class SiteRepositoryFirebaseImpl implements SiteRepository{
         return true;
     }
 
-    @CircuitBreaker(name = "firestoreCb", fallbackMethod = "getAllFallback")
     @Override
     public List<Site> getAll() throws ExecutionException, InterruptedException {
         return firestoreClient.collection(SITES_COLLECTION)
@@ -82,7 +74,6 @@ public class SiteRepositoryFirebaseImpl implements SiteRepository{
                 .toList();
     }
 
-    @CircuitBreaker(name = "firestoreCb", fallbackMethod = "getByUserIdFallback")
     @Override
     public List<Site> getByUserId(String userId) throws ExecutionException, InterruptedException {
         return firestoreClient.collection(SITES_COLLECTION)
@@ -93,35 +84,5 @@ public class SiteRepositoryFirebaseImpl implements SiteRepository{
                 .stream()
                 .map(queryDocumentSnapshot -> queryDocumentSnapshot.toObject(Site.class))
                 .toList();
-    }
-
-    public Site saveFallback(Site site, Throwable error) throws ExecutionException, InterruptedException {
-        log.error("Error saving site: {}", error.getMessage());
-        throw new FirestoreUnavailableException("Firestore is unavailable. Cannot save site.", error);
-    }
-
-    public Optional<Site> getByNameFallback(String name, Throwable error) throws ExecutionException, InterruptedException {
-        log.error("Error retrieving site by name: {}", error.getMessage());
-        throw new FirestoreUnavailableException("Firestore is unavailable. Cannot retrieve site by name.", error);
-    }
-
-    public Optional<Site> updateFallback(String name, Site updatedSite, Throwable error) throws ExecutionException, InterruptedException {
-        log.error("Error updating site: {}", error.getMessage());
-        throw new FirestoreUnavailableException("Firestore is unavailable. Cannot update site.", error);
-    }
-
-    public Boolean deleteFallback(String name, Throwable error) throws ExecutionException, InterruptedException {
-        log.error("Error deleting site: {}", error.getMessage());
-        throw new FirestoreUnavailableException("Firestore is unavailable. Cannot delete site.", error);
-    }
-
-    public List<Site> getAllFallback(Throwable error) throws ExecutionException, InterruptedException {
-        log.error("Error retrieving all sites: {}", error.getMessage());
-        throw new FirestoreUnavailableException("Firestore is unavailable. Cannot retrieve all sites.", error);
-    }
-
-    public List<Site> getByUserIdFallback(String userId, Throwable error) throws ExecutionException, InterruptedException {
-        log.error("Error retrieving sites by userId: {}", error.getMessage());
-        throw new FirestoreUnavailableException("Firestore is unavailable. Cannot retrieve sites by userId.", error);
     }
 }
--- src/main/java/com/green/energy/tracker/cloud/sites_service/service/SiteServiceImpl.java
+++ src/main/java/com/green/energy/tracker/cloud/sites_service/service/SiteServiceImpl.java
@@ -1,8 +1,11 @@
 package com.green.energy.tracker.cloud.sites_service.service;
 
+import com.green.energy.tracker.cloud.sites_service.exception.FirestoreUnavailableException;
 import com.green.energy.tracker.cloud.sites_service.model.Site;
 import com.green.energy.tracker.cloud.sites_service.repository.SiteRepository;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import java.time.Instant;
 import java.util.List;
@@ -12,10 +15,12 @@ import java.util.concurrent.ExecutionException;
 
 @Service("SiteServiceV1")
 @RequiredArgsConstructor
+@Slf4j
 public class SiteServiceImpl implements SiteService{
 
     private final SiteRepository siteRepository;
 
+    @CircuitBreaker(name = "firestoreCb", fallbackMethod = "saveFallback")
     @Override
     public Site save(String userId, String name, String description, String location) throws ExecutionException, InterruptedException {
         var siteId = UUID.randomUUID().toString();
@@ -29,31 +34,67 @@ public class SiteServiceImpl implements SiteService{
                 .creationDate(now)
                 .lastUpdateDate(now)
                 .build();
-        return siteRepository.save(site);
+        throw new RuntimeException("Simulated failure");
+        //return siteRepository.save(site);
     }
 
+    @CircuitBreaker(name = "firestoreCb", fallbackMethod = "getByNameFallback")
     @Override
     public Optional<Site> getByName(String name) throws ExecutionException, InterruptedException {
         return siteRepository.getByName(name);
     }
 
+    @CircuitBreaker(name = "firestoreCb", fallbackMethod = "updateFallback")
     @Override
     public Optional<Site> update(String name, Site updatedSite) throws ExecutionException, InterruptedException {
         return siteRepository.update(name, updatedSite);
     }
 
+    @CircuitBreaker(name = "firestoreCb", fallbackMethod = "deleteFallback")
     @Override
     public Boolean delete(String name) throws ExecutionException, InterruptedException {
         return siteRepository.delete(name);
     }
 
+    @CircuitBreaker(name = "firestoreCb", fallbackMethod = "getAllFallback")
     @Override
     public List<Site> getAll() throws ExecutionException, InterruptedException {
         return siteRepository.getAll();
     }
 
+    @CircuitBreaker(name = "firestoreCb", fallbackMethod = "getByUserIdFallback")
     @Override
     public List<Site> getByUserId(String userId) throws ExecutionException, InterruptedException {
         return siteRepository.getByUserId(userId);
+    }
+
+    public Site saveFallback(String userId, String name, String description, String location, Throwable error) throws ExecutionException, InterruptedException {
+        log.error("Error saving site: {}", error.getMessage());
+        throw new FirestoreUnavailableException("Firestore is unavailable. Cannot save site.", error);
+    }
+
+    public Optional<Site> getByNameFallback(String name, Throwable error) throws ExecutionException, InterruptedException {
+        log.error("Error retrieving site by name: {}", error.getMessage());
+        throw new FirestoreUnavailableException("Firestore is unavailable. Cannot retrieve site by name.", error);
+    }
+
+    public Optional<Site> updateFallback(String name, Site updatedSite, Throwable error) throws ExecutionException, InterruptedException {
+        log.error("Error updating site: {}", error.getMessage());
+        throw new FirestoreUnavailableException("Firestore is unavailable. Cannot update site.", error);
+    }
+
+    public Boolean deleteFallback(String name, Throwable error) throws ExecutionException, InterruptedException {
+        log.error("Error deleting site: {}", error.getMessage());
+        throw new FirestoreUnavailableException("Firestore is unavailable. Cannot delete site.", error);
+    }
+
+    public List<Site> getAllFallback(Throwable error) throws ExecutionException, InterruptedException {
+        log.error("Error retrieving all sites: {}", error.getMessage());
+        throw new FirestoreUnavailableException("Firestore is unavailable. Cannot retrieve all sites.", error);
+    }
+
+    public List<Site> getByUserIdFallback(String userId, Throwable error) throws ExecutionException, InterruptedException {
+        log.error("Error retrieving sites by userId: {}", error.getMessage());
+        throw new FirestoreUnavailableException("Firestore is unavailable. Cannot retrieve sites by userId.", error);
     }
 }
